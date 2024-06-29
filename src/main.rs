d1c0ca6abbe0be610fbe1344d6ca4c8fd8a6d4f6
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-06-29 16:05:38 +0200

    passing db pool

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use rust_okapi_template::routes;
+use std::sync::Arc;
+
+use rust_okapi_template::{db::connection::create_pool, routes};
 
 #[macro_use]
 extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![routes::todos::get_todos])
+    let db_pool = create_pool();
+
+    rocket::build()
+        .manage(Arc::new(db_pool))
+        .mount("/", routes![routes::todos::get_todos])
 }
